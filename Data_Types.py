76a28e37c5caaed7ee9ca7c73ad7d6a001089f7b
revathikuttan03 <revathikuttan03@gmail.com>
2026-03-09 10:04:42 +0530

    python basics

---
+++ Data_Types.py
@@ -0,0 +1,116 @@
+# data_1=True
+# print(type(data_1))
+# data=False
+# print(type(data))
+
+# a=10
+# b=20
+# c=a+b
+# print(c)
+
+# a=10
+# b=20
+# print(a+b)
+
+# a=10
+# b=20
+# c=a-b
+# print(c)
+
+# -10
+
+# a=10
+# b=20
+# c=a/b
+# print(c)
+
+# 0.5
+
+# a=10
+# b=20
+# c=a*b
+# print(c)
+
+# 200
+# Arithematic Operstions
+
+# Exponent
+
+# a=3
+# b=2
+# c=a**b
+# print(c)
+
+# 9
+
+# Modules
+
+# a=10
+# b=6
+# c=a%b
+# print(c)
+
+# 4
+
+# a=10
+# b=5
+# c=a%b
+# print(c)
+
+# 4
+
+# Floor Division 
+# a=10
+# b=3
+# c=10//3
+# print(c)
+
+# 3
+
+# Type Casting
+
+# a="10"
+# b="20"
+# c=a+b
+# print(c)
+
+# 1020
+
+# a='10'
+# a='20'
+# b='20'
+# c=a+b
+# print(c)
+
+# 2020
+
+#Type Casting
+# a="10"
+# a=int(a)
+# b="20"
+# b=int(b)
+# c=a+b
+# print(c)
+
+# 30
+
+# num1=int(input('enter your number'))
+# num2=int(input('enter your number'))
+# print((num1+num2))
+
+# 30
+
+# num1=input('enter your number')
+# num1=int(num1)
+# num2=input('enter your number')
+# num2=int(num2)
+# print(num1+num2)
+
+# 30
+
+name=input("enter your name:")
+# dob=input("enter your birth year:")
+dob=int(input("enter your birth year"))
+print("name:",name)
+age=2026-dob
+print("age:",age)
---
+++ Operators.py
@@ -0,0 +1,312 @@
+# a=10
+# b=10
+# c=a==b
+# print(c)
+
+# comparison operators
+# == equal to operator
+# a=10
+# b=20
+# c=a==b
+# print(c)
+
+# != not equal to
+
+# a=20
+# b=10
+# c=a!=b
+# print(c)
+
+# True
+
+# < less than
+
+# a=10
+# b=20
+# c=a < b
+# print(c)
+
+# graterthan
+
+# a=20
+# b=10
+# c=a>b
+# print(c)
+
+# <= lessthan or equals to
+
+# a=10
+# b=10
+# c=a<=b
+# print(c)
+
+# true
+
+# a=20
+# b=10
+# c=a<=b
+# print(c)
+
+# False
+
+# a=10
+# b=20
+# c=a<=b
+# print(c)
+
+# true 
+
+# graterthan equal
+
+# a=10
+# b=10
+# c=a>=b
+# print(c)
+
+# true
+# a=10
+# b=20
+# c=a>=b
+# print(c)
+
+# false
+
+# assign operators
+
+# a=10
+# b=10
+# a=20
+# print(a)
+
+# result 20
+
+# add and assign oprtr
+
+# a=10
+# b=10
+# a+=20
+# print(a)
+
+# 30
+
+# a=10
+# b=10
+# a+=20
+# a+=20
+# print(a)
+
+# output 50
+
+# substract and assign oprtr
+# a=50
+# a-=20
+# a-=10
+# print(a)
+
+# output 20
+
+# a=10
+# a-=20
+# a-=10
+# print(a)
+
+# output -20
+
+# division and assgn optr
+# a=50
+# a/=2
+# a/=2
+# print(a)
+
+# output 12.5
+
+# multiplication and asgn optr
+# a=50
+# a*=2
+# a*=2
+# print(a)
+
+# output 200
+
+# exponent and asgn optr
+
+# a=5
+# a**=2
+# a**=2
+# print(a)
+
+# output 625
+
+# module and asgn optr
+# a=50
+# a%=2
+# a%=2
+# print(a)
+
+# output 0
+
+# floor and asgn optr
+# a=5
+# a//=2
+# a//=2
+# print(a)
+
+# output 1
+
+# LOGICAL OPERATORS
+
+# a=10
+# b=20
+# c=a==b and a<=b
+# print(c)
+
+# output false
+
+# a=10
+# b=10
+# c=a==b and a<=b
+# print(c)
+
+# output true
+
+# w=35
+# c=w>=35 and w<=45
+# print(c)
+
+# true
+
+# w=55
+# c=w>=55 and w<=65
+# print(c)
+
+# true
+
+# w=40
+# c=w>=40 and w<=60
+# print(c) 
+
+# true
+
+# a=18
+# b=a>=18 and a<=35
+# print(b)
+
+# output True
+
+# length=int(input("enter your rectangle length"))
+# width=int(input("enter your rectange width"))
+# print("length",length)
+# print("width",width)
+# area=length*width
+# print(area)
+
+# age = 30
+# r=18>age or 40<age
+# print(r)
+
+# age = 15
+# r=18>age or 40<age
+# print(r)
+
+# age = 45
+# r=18>age or 40<age
+# print(r)
+
+# p= not True
+# print(p)
+
+# output False
+# age=17
+# p= not (age<18)
+# print(p)
+
+# output False
+
+# age=20
+# p= age>18 and age<35
+# print(not p)
+
+# output False
+
+# x=["apple","grape"]
+# y=["apple","grape"]
+# z=x
+# a= x is z
+# print(a)
+# print(z)
+
+# output 
+# True
+# ['apple', 'grape']
+
+# x=["apple","grape"]
+# y=["apple","grape"]
+# z=x
+# a= x is not z
+# print(a)
+
+# output False
+
+# membership operator
+# a="python is programming language"
+# b="programming" in a
+# print(b)
+
+# output True
+
+# a="python is programming language"
+# b="java" in a
+# print(b)
+
+# output False
+
+# a=10+20*2
+# print(a)
+
+# r=int(input("enter your number"))
+# b=3.14*(r**2)
+# print(b)
+
+# output 341.0
+# 
+# mark=20
+# if 70<mark:
+#     print("grade A")
+# elif 60< mark:
+#     print("garde B")
+# elif 50< mark:
+#     print("grade C")
+# else:
+#     print("your fail")
+
+# output your fail
+
+
+# mark =75
+# if mark<80:
+#     if 70<mark:
+#         print("grade A")
+# else:
+#     print("invalid input")
+
+# output grade A
+
+grade= input("enter your grade")
+match grade:
+    case "A" | "a":
+        print("70-80")
+    
+    case "B" :
+        print("60-70")
+    case "C":
+        print("50-60")
+    case "D":
+        print("40-50")
+    case "F":
+        print("0-30")
+    case _:
+        print("invalid input")
+
+
+
+        
